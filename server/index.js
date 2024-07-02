c91776588b0dd77ce28db227cbeb321c412d4116
Aashutosh Dahal <165886572+AashutoshDL@users.noreply.github.com>
2024-07-02 11:53:07 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "https://localhost:3000",
+  origin: "https://pms-client-nine.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
