d889a560142af2107896a61e9e3afde0487bbb5a
Aashutosh Dahal <165886572+AashutoshDL@users.noreply.github.com>
2024-07-04 14:37:28 +0545

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import SkillMuseum from './components/skillMuseum/SkillMuseum';
-import Events from './components/Events/Events';
 import Projects from './components/Projects/Projects';
 import AddProjects from './components/Projects/AddProjects';
 import {ToastContainer} from "react-toastify"
 import UpdateProjects from './components/Projects/UpdateProjects';
-
 function App() {
   return (
     <Router>
       <ToastContainer />
-      {/* <Navbar /> */}
       <Routes>
       <Route path='/' element={<SkillMuseum />} />
       <Route path='/events' element={<Events />} />
@@ -22,5 +19,4 @@ function App() {
     </Router>
   )
 }
-
 export default App
